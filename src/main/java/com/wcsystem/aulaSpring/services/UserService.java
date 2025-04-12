d55b478219a1ef5a62c8689cd15251affa7140f4
WagnerCSN <wagcostvez@gmail.com>
2025-04-12 17:43:15 -0300

    Service layer, component registration

---
+++ src/main/java/com/wcsystem/aulaSpring/services/UserService.java
@@ -0,0 +1,26 @@
+package com.wcsystem.aulaSpring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wcsystem.aulaSpring.entities.User;
+import com.wcsystem.aulaSpring.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
