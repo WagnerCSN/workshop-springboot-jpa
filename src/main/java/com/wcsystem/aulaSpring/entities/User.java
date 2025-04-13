36409c08d5d22ce5411b1eb9dcc4967970595b0e
WagnerCSN <wagcostvez@gmail.com>
2025-04-12 22:01:21 -0300

    Order, Instant, ISO 8601 - Parte 1

--- src/main/java/com/wcsystem/aulaSpring/entities/User.java
+++ src/main/java/com/wcsystem/aulaSpring/entities/User.java
@@ -1,12 +1,15 @@
 package com.wcsystem.aulaSpring.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -22,16 +25,20 @@ public class User implements Serializable{
 	private String phone;
 	private String password;
 	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+	
 	public User(){
 		
 	}
 
-	public User(Long id, String name, String email, String phone, String password) {
+	public User(Long id, String name, String email, String phone, String password, List<Order> orders) {
 		this.id = id;
 		this.name = name;
 		this.email = email;
 		this.phone = phone;
 		this.password = password;
+		this.orders = orders;
 	}
 
 	public Long getId() {
@@ -72,6 +79,10 @@ public class User implements Serializable{
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override
