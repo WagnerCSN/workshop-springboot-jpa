df5682c1fa8c0e4159f2232a8b848ed041ce3106
WagnerCSN <wagcostvez@gmail.com>
2025-04-26 20:40:22 -0300

    Feat: Payment, one-to-one association

--- src/main/java/com/wcsystem/aulaSpring/config/TestConfig.java
+++ src/main/java/com/wcsystem/aulaSpring/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.wcsystem.aulaSpring.entities.Category;
 import com.wcsystem.aulaSpring.entities.Order;
 import com.wcsystem.aulaSpring.entities.OrderItem;
+import com.wcsystem.aulaSpring.entities.Payment;
 import com.wcsystem.aulaSpring.entities.Product;
 import com.wcsystem.aulaSpring.entities.User;
 import com.wcsystem.aulaSpring.entities.enums.OrderStatus;
@@ -79,6 +80,9 @@ public class TestConfig implements CommandLineRunner{
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));	
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
 		
+		orderRepository.save(o1);
 	}
 }
