5f812bc66fd10b4f9b08d529c372cddb0de98c87
WagnerCSN <wagcostvez@gmail.com>
2025-05-10 18:18:43 -0300

    Feat: User update

--- src/main/java/com/wcsystem/aulaSpring/services/UserService.java
+++ src/main/java/com/wcsystem/aulaSpring/services/UserService.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Service;
 import com.wcsystem.aulaSpring.entities.User;
 import com.wcsystem.aulaSpring.repositories.UserRepository;
 
+import jakarta.transaction.Transactional;
+
 @Service
 public class UserService {
 	
@@ -30,5 +32,18 @@ public class UserService {
 	
 	public void delete(Long id) {
 		repository.deleteById(id);
+	}
+	
+	@Transactional
+	public User update(Long id, User obj) {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+	}
+	
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
 	}
 }
