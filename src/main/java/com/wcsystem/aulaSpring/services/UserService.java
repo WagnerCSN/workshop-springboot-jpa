b1e5b51924ae985bb1116a7d1448dca5f3a57b72
WagnerCSN <wagcostvez@gmail.com>
2025-05-18 22:02:24 -0300

    Feat: Exception handling - update

--- src/main/java/com/wcsystem/aulaSpring/services/UserService.java
+++ src/main/java/com/wcsystem/aulaSpring/services/UserService.java
@@ -13,6 +13,7 @@ import com.wcsystem.aulaSpring.repositories.UserRepository;
 import com.wcsystem.aulaSpring.services.exceptions.DatabaseException;
 import com.wcsystem.aulaSpring.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 
 @Service
@@ -51,9 +52,14 @@ public class UserService {
 	
 	@Transactional
 	public User update(Long id, User obj) {
-			User entity = repository.getReferenceById(id);
-			updateData(entity, obj);
-			return repository.save(entity);
+			try {
+				User entity = repository.getReferenceById(id);
+				updateData(entity, obj);
+				return repository.save(entity);
+			}
+			catch(EntityNotFoundException e) {
+				throw new ResourceNotFoundException(id);
+			}
 	}
 	
 	private void updateData(User entity, User obj) {
